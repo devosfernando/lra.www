84afc58d37e9df500640f4a2dc9c7be91b863361
Juan Carlos Coronado Bonilla <coronadobonillajuancarlos904@gmail.com>
2023-06-08 18:03:23 -0500

    version de control

--- data/key.js
+++ data/key.js
@@ -1,4 +1,21 @@
-var firebaseConfig = {
+function dataFront() {
+
+  let dataFront ;
+
+  $.ajax({
+    url: `http://host.docker.internal:8080/api/1/auth/parametry/data`,
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      console.log(data)
+      dataFront = data
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus, errorThrown); // Imprimimos el error en la consola en caso de fallar
+    },
+  });
+
+  var firebaseConfig = {
     apiKey: "AIzaSyBWmy9wW0K2B8K8dKI99UeALrptJqYf8s4",
     authDomain: "planbackend-38c65.firebaseapp.com",
     projectId: "planbackend-38c65",
@@ -8,8 +25,11 @@ var firebaseConfig = {
     measurementId: "G-97V65Z4ZC1",
   };
 
-  let lol = process.env.LOL
-  console.log(lol)
+  let lol = process.env.LOL;
+  console.log(lol);
 
   firebase.initializeApp(firebaseConfig);
   const auth = firebase.auth();
+}
+
+dataFront();
